62619e7f3303a7e77e630b01844a96aadbd395fd
timothyckl <timothy.ckl@outlook.com>
2024-03-21 22:40:05 +0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="iotadb",
+    version="0.0.0",
+    description="",
+    long_description="# IotaDB",
+    url="",
+    author="timothyckl",
+    author_email="timothy.ckl@outlook.com",
+    packages=[],
+    classifiers=[
+        "Development Status :: 1 - Planning"
+    ]
+)
